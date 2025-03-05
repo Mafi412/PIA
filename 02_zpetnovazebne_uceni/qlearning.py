967d135e9eafdd6e70b525f8ea32a0c6523f0017
Matyáš Lorenc <Mafi412@seznam.cz>
2025-03-05 16:55:55 +0100

    Q-learning

--- 02_zpetnovazebne_uceni/qlearning.py
+++ 02_zpetnovazebne_uceni/qlearning.py
@@ -5,14 +5,12 @@ import matplotlib.pyplot as plt
 
 
 class QAgent:
-    def __init__(self, action_space, epsilon=0.01, alpha=0.1, gamma=1.):
+    def __init__(self, action_space, epsilon=0.01, alpha=0.1, gamma=0.9):
         self.action_space = action_space
         
         self.epsilon = epsilon
         self.alpha = alpha
         self.gamma = gamma
-        
-        self.train = True
         
         num_bins_position = 12 # 12 subintervals for the position
         num_bins_velocity = 8 # 8 subintervals for the velocity
@@ -20,70 +18,60 @@ class QAgent:
         self.bins_velocity = np.linspace(-0.07, 0.07, num_bins_velocity + 1)
         
         self.Q = np.zeros((num_bins_position, num_bins_velocity, 3)) # Q[position, velocity, action]
-        self.last_state = None
-        self.last_action = None
+        
+        self.greedy = False
         
         
-    def _discretize(self, observation):
-        state_position = np.digitize(observation[0], self.bins_position) - 1
-        state_velocity = np.digitize(observation[1], self.bins_velocity) - 1
+    def _discretize(self, state):
+        state_position = np.digitize(state[0], self.bins_position) - 1
+        state_velocity = np.digitize(state[1], self.bins_velocity) - 1
         return (state_position, state_velocity)
     
     
-    def act(self, observation, reward, done):
-        current_state = self._discretize(observation)
+    def act(self, state):
+        current_state = self._discretize(state)
         
-        if self.train:
-            # Training
-            if self.last_state != None:
-                self.Q[self.last_state[0], self.last_state[1], self.last_action] = \
-                    self.alpha * (reward + self.gamma * np.max(self.Q[current_state[0], current_state[1], :])) + \
-                    (1 - self.alpha) * self.Q[self.last_state[0], self.last_state[1], self.last_action]
-                
-            # Action selection
-            if np.random.random() >= self.epsilon:
-                chosen_action = np.argmax(self.Q[current_state[0], current_state[1], :])
-            
-            else:
-                chosen_action = self.action_space.sample()
-                
-            # Update of the inner memory for training
-            self.last_state = current_state
-            self.last_action = chosen_action
-            
-            return chosen_action
+        if not self.greedy and np.random.random() < self.epsilon:
+            chosen_action = self.action_space.sample()
         
         else:
-            return np.argmax(self.Q[current_state[0], current_state[1], :])
-    
-    
-    def reset(self):
-        self.last_state = None
-        self.last_action = None
+            chosen_action = np.argmax(self.Q[current_state[0], current_state[1], :])
+            
+        return chosen_action
+        
+        
+    def train(self, state, action, reward, next_state, terminated):
+        state, next_state = self._discretize(state), self._discretize(next_state)
+        
+        if not terminated:
+            rest_of_the_episode_estimation = np.max(self.Q[next_state[0], next_state[1], :])
+        else:
+            rest_of_the_episode_estimation = 0.
+        
+        self.Q[state[0], state[1], action] += \
+            self.alpha * (reward + self.gamma * rest_of_the_episode_estimation - self.Q[state[0], state[1], action])
     
 
 env = gymnasium.make("MountainCar-v0")
-
 agent = QAgent(env.action_space)
 
 # Training
-agent.train = True
+print("Training the agent...")
+agent.greedy = False
+
 total_returns = []
 for _ in range(1000):
-    observation, _ = env.reset()
-    agent.reset()
+    state, _ = env.reset()
     done = False
-    
-    reward = 0.
     R = 0. # return
-    timestep = 0
     
     while not done:
-        action = agent.act(observation, reward, done)
-        observation, reward, terminated, truncated, _ = env.step(action)
+        action = agent.act(state)
+        next_state, reward, terminated, truncated, _ = env.step(action)
+        agent.train(state, action, reward, next_state, terminated)
+        state = next_state
         done = terminated or truncated
         R += reward
-        timestep += 1
         
     total_returns.append(R)
 
@@ -93,5 +81,6 @@ plt.plot(utils.moving_average(total_returns, 10))
 plt.show()
 
 # Evaluation
-agent.train = False
-print("Obtained returns:", utils.simulate(agent, "MountainCar-v0", steps=200, episodes=5))
+print("Evaluating the trained agent...")
+agent.greedy = True
+print("Obtained returns:", utils.simulate(agent, "MountainCar-v0", steps=200, episodes=3))
